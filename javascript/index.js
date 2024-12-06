8b95e6ad81e8e150790aea64ce6e6a5c5de91da4
Eliace <tacalada@3il.fr>
2024-12-06 05:56:27 +0100

    ajout de tous les fichiers dans html

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-const image = document.getElementById('image');
-const link = document.getElementById('moveLink');
-
-link.addEventListener('click', function(e) {
-  e.preventDefault();  // Empêche l'action par défaut du lien
-  image.classList.toggle('moving'); // Ajoute ou enlève la classe "moving"
-});
